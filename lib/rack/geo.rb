df43dee391c227fa660517e2465e8f76d81714bb
Matt Patterson <matt@reprocessed.org>
2011-03-26 20:39:07 +0000

    Add .alphagov.co.uk domain to geo cookie when in production mode

--- lib/rack/geo.rb
+++ lib/rack/geo.rb
@@ -27,7 +27,10 @@ module Rack
       status, headers, body = @app.call(env)
 
       response = Rack::Response.new(body, status, headers)
-      response.set_cookie('geo', encoded_geo)
+      geo_cookie = {:value => encoded_geo}
+      # geo_cookie.merge!(:domain => '.alphagov.co.uk', :path => '/') if ENV['RACK_ENV'] == 'production'
+      geo_cookie.merge!(:domain => 'matt.alphagov.co.uk', :path => '/')
+      response.set_cookie('geo', geo_cookie)
       response.finish
     end
 

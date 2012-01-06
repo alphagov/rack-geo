7dad171a52818eaf65db727de6eb378840e68581
Gareth Rushgrove <gareth@morethanseven.net>
2012-01-06 18:56:57 +0000

    these tests had been left in after the functionality had been stripped out by someone

--- spec/rack/geo_spec.rb
+++ spec/rack/geo_spec.rb
@@ -50,76 +50,8 @@ describe Rack::Geo do
         Utils.decode_stack(harness.env['HTTP_X_GOVGEO_STACK']).should == {'GEOSTACK' => 'ENCODED'}
       end
 
-      it "should add a Geo cookie to the response" do
-        last_response_cookies.should have_key('geo')
-        Utils.decode_stack(last_response_cookies['geo']).should == {'GEOSTACK' => 'ENCODED'}
-      end
-    end
-  end
-
-  describe "A returning visitor" do
-    before(:each) do
-      @stack_hash = {'postcode' => 'w1a 1', 'fuzzy_point' => {'lat' => '0.0', 'lon' => '0.0', 'accuracy' => 'planet'}}
-      current_session.set_cookie("geo=#{Utils.encode_stack(@stack_hash)}; domain=.example.org; path=/")
-    end
-
-    it "should be given a geostack from their cookie, on get request" do
-      Geogov::GeoStack.stubs(:new_from_hash).with do |stack|
-        stack['postcode'].should == 'w1a 1'
-      end.returns(@geostack)
-      get "/"
-    end
-
-    it "should be given a geostack from their cookie, on post request" do
-      Geogov::GeoStack.stubs(:new_from_hash).with do |stack|
-        stack['postcode'].should == 'w1a 1'
-      end.returns(@geostack)
-      post "/"
-    end
-
-    context "and rack-geo" do
-      before(:each) do
-        get "/"
-      end
-
-      it "should add a Geo header to the env for apps further down the chain" do
-        harness.env.should have_key('HTTP_X_GOVGEO_STACK')
-        JSON.parse(Base64.decode64(harness.env['HTTP_X_GOVGEO_STACK'])).should == @stack_hash
-      end
-
-      it "should add a Geo cookie to the response" do
-        last_response_cookies.should have_key('geo')
-        JSON.parse(Base64.decode64(last_response_cookies['geo'])).should == @stack_hash
-      end
-    end
-  end
-
-  describe "A visitor giving extra geo data" do
-    before(:each) do
-      @new_stack = Geogov::GeoStack.new_from_hash({'fuzzy_point' => {'lat' => 0, 'lon' => 0, 'accuracy' => :planet}})
-      Geogov::GeoStack.stubs(:new_from_hash).with('postcode' => 'W12 7RJ').returns(@geostack)
-      current_session.set_cookie("geo=#{Utils.encode_stack({'postcode' => 'W12 7RJ'})}; domain=example.org; path=/")
-    end
-
-    it "should be given a geostack updated with new params" do
-      @geostack.expects(:update).with('postcode' => 'W1A 1AA').returns(@new_stack)
-      post "/", "postcode" => "W1A 1AA"
-    end
-
-    context "and rack-geo" do
-      before(:each) do
-        @geostack.stubs(:update).with('postcode' => 'W1A 1AA').returns(@new_stack)
-        post "/", "postcode" => "W1A 1AA"
-      end
-
-      it "should add a Geo header to the env for apps further down the chain" do
-        harness.env.should have_key('HTTP_X_GOVGEO_STACK')
-        harness.env['HTTP_X_GOVGEO_STACK'].should == Utils.encode_stack(@new_stack.to_hash)
-      end
-
-      it "should add a Geo cookie to the response" do
-        last_response_cookies.should have_key('geo')
-        last_response_cookies['geo'].should == Utils.encode_stack(@new_stack.to_hash)
+      it "should no longer add a Geo cookie to the response" do
+        last_response_cookies.should_not have_key('geo')
       end
     end
   end
@@ -140,10 +72,9 @@ describe Rack::Geo do
         end
       end
 
-      it "should return the cookie" do
+      it "should no longer return the cookie" do
         post "/locator.json", :postcode => "W1A 1AA"
-        last_response_cookies.should have_key('geo')
-        last_response_cookies['geo'].should_not be_nil
+        last_response_cookies.should_not have_key('geo')
       end
 
       it "should not call the app Rack-geo is wrapping" do

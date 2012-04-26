429647ed6ac82dca2d478e7f5e5e62155a1d6d8d
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-04-26 05:50:44 -0700

    Fix jenkins build.
    Gemfile.lock is not in source control, therefore we should clean it up
    before doing bundle install

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -x
+
+# Gemfile.lock is not in source control because this is a gem.
+rm -f Gemfile.lock
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 bundle exec rake spec
 # cd example && bundle install && bundle exec rake test
